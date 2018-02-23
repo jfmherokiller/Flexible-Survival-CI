470f00386f2e15746b438283cb918c1dedf32e32
noah the goodra <peterpan0413@live.com>
2018-02-23 14:46:17 -0600

    move back outside because undefined

--- build-code.sh
+++ build-code.sh
@@ -2,4 +2,4 @@
 
 #$I7_PREFIX/bin/ni -internal $I7_PREFIX/share/inform7/Internal -project FlexibleSurvival.inform -format=ulx
 #$I7_PREFIX/bin/inform6 -kE2SDwG "+include_path=,.,../Source" "FlexibleSurvival.inform/Build/auto.inf" "FlexibleSurvival.inform/Build/output.ulx"
-travis_wait $I7_PREFIX/bin/i7 -p $I7_PREFIX -r FlexibleSurvival.inform
+$I7_PREFIX/bin/i7 -p $I7_PREFIX -r FlexibleSurvival.inform
