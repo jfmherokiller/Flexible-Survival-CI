650d0ec59e641c43d2e14e6484007ab5c6938c0c
noah the goodra <peterpan0413@live.com>
2018-02-24 17:33:16 -0600

    replace script and update extensions

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 set -e
 
 ./install-inform.sh $I7_PREFIX
+#replace i7 script with my edited version
+curl https://gist.githubusercontent.com/jfmherokiller/e52c873a476eb1fbc9e032512e91d3e7/raw/b59f3e3e8b06cfb37d78f78787498f14bb724448/i7.pl > $I7_PREFIX/bin/i7
+chmod +x $I7_PREFIX/bin/i7
 
 cp ./FlexibleSurvival.materials/Extensions/Inform/story.ni ./FlexibleSurvival.inform/Source/story.ni
 cp -r ./FlexibleSurvival.materials/Extensions/Figures ./FlexibleSurvival.materials/
