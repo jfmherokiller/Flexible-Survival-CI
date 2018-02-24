967b3aa12100633b3e141c36ba068e2912261705
noah the goodra <peterpan0413@live.com>
2018-02-24 14:35:47 -0600

    forgot to run build in background

--- build-code.sh
+++ build-code.sh
@@ -6,7 +6,7 @@
 # Look for the 4 common signals that indicate this script was killed.
 # If the background command was started, kill it, too.
 trap '[ -z $! ] || kill $!' SIGHUP SIGINT SIGQUIT SIGTERM
-$I7_PREFIX/bin/i7 -p $I7_PREFIX -r FlexibleSurvival.inform #compile flexible survival
+$I7_PREFIX/bin/i7 -p $I7_PREFIX -r FlexibleSurvival.inform & #compile flexible survival
 # The /proc directory exists while the command runs.
 while [ -e /proc/$! ]; do
     echo -n "."  # Do something while the background command runs.
