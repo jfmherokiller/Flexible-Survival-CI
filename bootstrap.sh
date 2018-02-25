5092b1e01215083923c4c7a7d73cf8f48e6838d2
noah the goodra <peterpan0413@live.com>
2018-02-25 11:49:46 -0600

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 set -e
 
 #allow for caching of the inform7 directory
-if [ ! -d "$I7_PREFIX" ]; then
+if [ ! -d "$I7_PREFIX/bin" ]; then
   ./install-inform.sh $I7_PREFIX
   #replace i7 script with my edited version
   curl https://gist.githubusercontent.com/jfmherokiller/e52c873a476eb1fbc9e032512e91d3e7/raw/b59f3e3e8b06cfb37d78f78787498f14bb724448/i7.pl > $I7_PREFIX/bin/i7
